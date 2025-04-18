b34efb28c267bd410f96f61acd160f5a35d56e1c
5006222 <5006222@mitacsc.edu.in>
2025-04-18 08:49:57 +0530

    Add files via upload

---
+++ Slip_4_9_14_Q1_lifecycle/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.slip_4_9;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showToast("onCreate");
+
+        Button btnNext = findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    @Override protected void onStart() {
+        super.onStart(); showToast("onStart");
+    }
+    @Override protected void onResume() {
+        super.onResume(); showToast("onResume");
+    }
+    @Override protected void onPause() {
+        super.onPause(); showToast("onPause");
+    }
+    @Override protected void onStop() {
+        super.onStop(); showToast("onStop");
+    }
+    @Override protected void onRestart() {
+        super.onRestart(); showToast("onRestart");
+    }
+    @Override protected void onDestroy() {
+        super.onDestroy(); showToast("onDestroy");
+    }
+
+    private void showToast(String methodName) {
+        Toast.makeText(this, TAG + " - " + methodName, Toast.LENGTH_SHORT).show();
+        Log.d(TAG, methodName + " called");
+    }
+}
